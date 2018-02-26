621cabbb2c8f8d7a8064df19a653d36c120185c8
wangsanfeng1998 <wangsanfeng1998@gmail.com>
2018-02-25 21:16:27 -0600

    added members to proc struct

--- kernel/proc.h
+++ kernel/proc.h
@@ -74,6 +74,8 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int isThread;                // Process = 0 and Thread = 1
+  struct spinlock lock;        // mutex
 };
 
 // Process memory is laid out contiguously, low addresses first:
